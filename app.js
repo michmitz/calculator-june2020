6db2452b725e990ec365e3004b0a1955fae92a48
Michelle Stermitz <stermitzm@gmail.com>
2020-06-25 14:17:25 -0700

    Created calculator, forgot to commit before this point...

--- app.js
+++ app.js
@@ -25,3 +25,69 @@ boogerButton.addEventListener('click', () => {
     // update the textContent of the span
     boogerSpan.textContent = sum;
 });
+
+const multiplyInput1 = document.getElementById('multiply-input-1');
+console.log('multiplyInput1');
+
+const multiplyInput2 = document.getElementById('multiply-input-2');
+console.log('multiplyInput2');
+
+const multiplyButton = document.getElementById('multiply-button');
+console.log('multiplyButton');
+
+const multiplySpan = document.getElementById('multiply-result');
+console.log('multiplySpan');
+
+//set the event handler to the multiply button
+multiplyButton.addEventListener('click', () => {
+    const mvalue1 = multiplyInput1.value;
+    console.log(mvalue1);
+    const mvalue2 = multiplyInput2.value;
+    console.log(mvalue2);
+    const result = Number(mvalue1) * Number(mvalue2);
+    multiplySpan.textContent = result;
+});
+
+
+const divisionInput1 = document.getElementById('division-input-1');
+console.log('DivisionInput1');
+
+const divisionInput2 = document.getElementById('division-input-2');
+console.log('DivisionInput2');
+
+const divisionButton = document.getElementById('division-button');
+console.log('divisionButton');
+
+const divisionResult = document.getElementById('division-result');
+console.log('divisionResult');
+
+divisionButton.addEventListener('click', () => {
+    const dvalue1 = divisionInput1.value;
+    console.log(dvalue1);
+    const dvalue2 = divisionInput2.value;
+    console.log(dvalue2);
+    const result = Number(dvalue1) / Number(dvalue2);
+    divisionResult.textContent = result;
+
+});
+
+const subtractionInput1 = document.getElementById('subtraction-input-1');
+console.log(subtractionInput1);
+
+const subtractionInput2 = document.getElementById('subtraction-input-2');
+console.log(subtractionInput2);
+
+const subtractionButton = document.getElementById('subtraction-button');
+console.log(subtractionButton);
+
+const subtractionResult = document.getElementById('subtraction-result');
+console.log(subtractionResult);
+
+subtractionButton.addEventListener('click', () => {
+    const svalue1 = subtractionInput1.value;
+    console.log(svalue1);
+    const svalue2 = subtractionInput2.value;
+    console.log(svalue2);
+    const result = Number(svalue1) - Number(svalue2);
+    subtractionResult.textContent = result;
+});
